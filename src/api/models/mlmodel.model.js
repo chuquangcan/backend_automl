9fd549ad0cc243507311a2b6d71849293dd0ba13
galaxyImpact204 <tranduytuana123@gmail.com>
2024-11-30 16:40:18 +0700

    :sparkles:feat: necessary model

---
+++ src/api/models/mlmodel.model.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    project_id: { type: Schema.Types.ObjectId, ref: "Project", required: true },
+    author_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true },
+    url: { type: String, required: true },
+
+    //additional info for predict
+    projectName: { type: String },
+    userEmail: { type: String },
+    runID: { type: String },
+  },
+  { timestamps: true }
+);
+
+const MLModel = model("MLModel", schema);
+export default MLModel;
